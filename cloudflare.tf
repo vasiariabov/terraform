ca40080f343732cf9bf0a6f523d7c9cbeb8bd12e
vasiariabov <71749308+vasiariabov@users.noreply.github.com>
2022-07-06 12:38:33 +0300

    Update cloudflare.tf

--- cloudflare.tf
+++ cloudflare.tf
@@ -21,3 +21,12 @@ resource "cloudflare_record" "www" {
   type    = "A"
   
 }
+
+
+resource "cloudflare_record" "joka" {
+  zone_id = "72217ecd07884307a08420b709cdd4d6"
+  name    = "joka"
+  value   = "10.10.113.10"
+  type    = "A"
+  
+}
