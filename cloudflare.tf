205311fc1ec8096b4f39e4f3c678804883bd715d
vasiariabov <71749308+vasiariabov@users.noreply.github.com>
2022-07-06 10:19:32 +0300

    Update cloudflare.tf

--- cloudflare.tf
+++ cloudflare.tf
@@ -17,7 +17,7 @@ provider "cloudflare" {
 resource "cloudflare_record" "www" {
   zone_id = "72217ecd07884307a08420b709cdd4d6"
   name    = "www"
-  value   = "203.0.113.10"
+  value   = "203.10.113.10"
   type    = "A"
   
 }
